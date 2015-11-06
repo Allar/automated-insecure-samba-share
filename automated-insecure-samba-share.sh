901259bc64b3c8025ed103f87d07ee5e8448ea11
Michael Allar <allar@michaelallar.com>
2015-11-06 03:55:46 -0800

    Added smb.conf url to wget call

--- automated-insecure-samba-share.sh
+++ automated-insecure-samba-share.sh
@@ -1,3 +1,4 @@
+#!/bin/bash 
 my_name=$(whoami)
 
 sudo apt-get update
@@ -8,9 +9,9 @@ sudo rm /etc/samba/smb.conf
 sudo mkdir /home/Drop
 sudo chown $my_name /home/Drop
 
-sudo wget placeholder --output-document=/etc/samba/smb.conf
+sudo wget https://cdn.rawgit.com/Allar/automated-insecure-samba-share/master/smb.conf --output-document=/etc/samba/smb.conf
 sudo sed -i 's/YOUR_USERNAME_HERE/$my_name/g' /etc/samba/smb.conf
 sudo sed -i 's/YOUR_SERVER_HOSTNAME_HERE/$HOSTNAME/g' /etc/samba/smb.conf
 
-sudo reboot
+# sudo reboot
 
